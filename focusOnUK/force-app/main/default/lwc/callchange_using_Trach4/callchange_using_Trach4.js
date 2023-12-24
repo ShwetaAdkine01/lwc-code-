3c3c20ffa9e6c21c727627c37e1f72992f22eaf4
Sushant Rode <sushant.rode@gmail.com>
2023-12-24 20:12:48 +0000

    First commit on branch

--- focusOnUK/force-app/main/default/lwc/callchange_using_Trach4/callchange_using_Trach4.js
+++ focusOnUK/force-app/main/default/lwc/callchange_using_Trach4/callchange_using_Trach4.js
@@ -1,48 +1,41 @@
-import { LightningElement,track } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Callchange_using_Trach4 extends LightningElement 
-{
-  @track showmecal;
-  num1;
-  num2;
+export default class Callchange_using_Trach4 extends LightningElement {
+   @track showmecal;
+   num1;
+   num2;
 
-  callme(event)
-  {
-    const eventname=event.target.name;
+   callme(event) {
+      const eventname = event.target.name;
 
-    if(eventname=='aval')
-    {
-    this.num1= event.target.value
-    }
-    else
-    {
-   this.num2=event.target.value
-    }
-  }
+      if (eventname == 'aval') {
+         this.num1 = event.target.value
+      }
+      else {
+         this.num2 = event.target.value
+      }
+   }
+   //   test
 
-  addme()
-{
-   const a= parseInt(this.num1);
-   const b= parseInt(this.num2);
-   this.showmecal='addition of two number is'+(a+b);  
-}
-subme()
-{
-   const a= parseInt(this.num1);
-   const b= parseInt(this.num2);
-   this.showmecal='substraction of two number is'+(a-b);  
-}
-mulme()
-{
-   const a= parseInt(this.num1);
-   const b= parseInt(this.num2);
-   this.showmecal='multiplication of two number is'+(a*b);  
-}
-divme()
-{
-   const a= parseInt(this.num1);
-   const b= parseInt(this.num2);
-   this.showmecal='division of two number is'+(a/b);  
-}
+   addme() {
+      const a = parseInt(this.num1);
+      const b = parseInt(this.num2);
+      this.showmecal = 'addition of two number is' + (a + b);
+   }
+   subme() {
+      const a = parseInt(this.num1);
+      const b = parseInt(this.num2);
+      this.showmecal = 'substraction of two number is' + (a - b);
+   }
+   mulme() {
+      const a = parseInt(this.num1);
+      const b = parseInt(this.num2);
+      this.showmecal = 'multiplication of two number is' + (a * b);
+   }
+   divme() {
+      const a = parseInt(this.num1);
+      const b = parseInt(this.num2);
+      this.showmecal = 'division of two number is' + (a / b);
+   }
 
 }
